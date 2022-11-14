d371b9ec357f3669dbfc496db8e304702fee6275
zoo-code <75787789+zooo-code@users.noreply.github.com>
2022-11-14 23:46:57 +0900

    spring interceptor

---
+++ toy/src/main/java/study/toy/WebConfig.java
@@ -0,0 +1,27 @@
+package study.toy;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import study.toy.interceptor.LogInterceptor;
+import study.toy.interceptor.LoginCheckInterceptor;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/" ,"/home" ,"/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error");
+    }
+}
