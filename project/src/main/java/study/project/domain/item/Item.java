749f09c2d8f936f2bac8265863b20c99010576ee
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-02-08 23:14:32 +0900

    item

--- project/src/main/java/study/project/domain/item/Item.java
+++ project/src/main/java/study/project/domain/item/Item.java
@@ -1,6 +1,8 @@
 package study.project.domain.item;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,6 +11,7 @@ import javax.persistence.Id;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Item {
     @Id @GeneratedValue
     @Column(name = "item_id")
@@ -19,4 +22,10 @@ public class Item {
     private int stockQuantity;
 
     private int price;
+
+    public Item(String itemName, int stockQuantity, int price) {
+        this.itemName = itemName;
+        this.stockQuantity = stockQuantity;
+        this.price = price;
+    }
 }
