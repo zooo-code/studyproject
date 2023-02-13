0bbba81f185a892f04a44bb4dd26b0e6dede73be
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-02-13 23:03:50 +0900

    controller start

---
+++ project/src/main/java/study/project/web/HomeController.java
@@ -0,0 +1,18 @@
+package study.project.web;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String Home(){
+
+        log.info("home");
+        return "home";
+    }
+}
