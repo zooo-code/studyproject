aebc8770ac59f28e3ee8336061f6a33a7bc7e6b2
zoo-code <75787789+zooo-code@users.noreply.github.com>
2023-05-11 21:42:44 +0900

    chat

--- project/src/main/java/study/project/chatWebSocket.java
+++ project/src/main/java/study/project/chatWebSocket.java
@@ -2,10 +2,10 @@ package study.project;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import study.project.domain.chat.WebSocketHandler;
 
 
 @RequiredArgsConstructor
@@ -16,6 +16,6 @@ public class chatWebSocket implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "ws/chat").setAllowedOrigins("*");
+        registry.addHandler(webSocketHandler, "/chat").setAllowedOrigins("*");
     }
 }
